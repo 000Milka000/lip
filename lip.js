91ce85366611c49c6e8ddb8a18b3a08fd58f0f21
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-10-31 11:43:13 +0300

    TTT

--- lip.js
+++ lip.js
@@ -1,3 +1,8 @@
 let mas = [2,48,5,6,9,91,71,24];
 console.log(mas.sort((a,b) => a-b));
-console.log(mas.sort())
+console.log(mas.sort());
+
+const isURL = require('isurl');
+
+console.log(isURL('http://domain/'));  //-> false
+console.log(isURL(new URL('http://domain/')));  //-> true
